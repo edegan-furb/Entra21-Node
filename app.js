e896adf06312c2004e99e70eb760dd53afe7cce8
Edegan <eduardord324@gmail.com>
2024-01-16 15:11:54 -0300

    Added New Routes in App.js

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ app.use(express.json());
 
 // Importar rotas
 const produtosRoutes = require('./routes/produtosRoutes');
+const entradasRoutes = require('./routes/entradasRoutes');
+const saidasRoutes = require('./routes/saidasRoutes');
 
 // Usar rotas
-app.use(produtosRoutes);
+app.use(produtosRoutes, entradasRoutes, saidasRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
